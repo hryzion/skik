92f88a2cb449d2b08fdf5ce1410929e8fd5d274b
hryzion <13651325353@sina.cn>
2024-09-19 11:27:18 +0800

    update

--- config.py
+++ config.py
@@ -2,3 +2,12 @@ num_epochs = 100
 learing_rate = 1e-4
 batch_size = 1
 num_classes=1 + 46
+
+
+
+
+# 参数设置
+initial_temp = 10000    # 初始温度
+cooling_rate = 0.99     # 冷却速率
+min_temp = 1            # 最小温度
+initial_solution = 0    # 初始解
--- simu.py
+++ simu.py
@@ -1,6 +1,7 @@
 import math
 import random
 import numpy as np
+from config import *
 
 # 目标函数，这里以寻找某个函数的最小值为例
 def objective_function(x):
@@ -60,7 +61,30 @@ def simulated_annealing_pos(sketch_feature,scene_json, init_view,photo2sketch_mo
 
 
 def simulated_annealing_rot(sketch_feature,scene_json, init_view,photo2sketch_model,swint_model):
-    view = init_view
+    view = np.array(init_view)
+    scene_sketch = scene2photo(scene_json,view)
+    scene_feature = inference(scene_sketch,True)
+    sketch_feature = np.array(sketch_feature)
+    scene_feature = np.array(scene_feature)
+    current_value = np.linalg.norm(sketch_feature,scene_feature)
+
+    temp = initial_temp
+    
+    while temp > min_temp:
+        new_view = view
+        new_view['pos'] = new_view['pos'] + (random.random() - 0.5) * temp * new_view['direction']
+        
+        scene_sketch = scene2photo(scene_json,view)
+        scene_feature = inference(scene_sketch,True)
+        scene_feature = np.array(scene_feature)
+        new_value = np.linalg.norm(sketch_feature,scene_feature)
+        
+        # 计算接受概率
+        if new_value < current_value or random.random() < math.exp((current_value - new_value) / temp):
+            view = new_view
+        
+        # 降温
+        temp *= cooling_rate
 
     
     
@@ -77,8 +101,3 @@ def simulated_annealing(sketch,scene_json, init_view,photo2sketch_model,swint_mo
 
 
 
-# 参数设置
-initial_temp = 10000    # 初始温度
-cooling_rate = 0.99     # 冷却速率
-min_temp = 1            # 最小温度
-initial_solution = 0    # 初始解
