ac0a91c6c5ae1b8bbccb664882e9e3b71343ad2e
step <step23716@gmail.com>
2024-09-21 09:37:50 +0800

    feat: initialize_view set y to 1.3

--- pipeline.py
+++ pipeline.py
@@ -16,7 +16,7 @@ initial_solution = 0    # 初始解
 '''
 根据scene_json 输出一个初始的视点 可以是房间包围盒的中心点 或者所有物体的重心
 ouput: {'pos':[x, y, z], 'direction':[dx, dy, dz]}
-pos: 房间包围盒的中心点
+pos: 房间包围盒的中心点 y设置为1.3
 direction: scene json的front方向
 '''
 
@@ -24,7 +24,12 @@ def initialize_view(scene_json:dict):
     bbox_dict: dict = scene_json.get('bbox', {})
     min_list: list = bbox_dict.get('min', [0, 0, 0])
     max_list: list = bbox_dict.get('max', [0, 0, 0])
-    pos_list: list = [(i+j)/2 for i, j in zip(min_list, max_list)]
+    # pos_list: list = [(i+j)/2 for i, j in zip(min_list, max_list)]
+    pos_list: list = [
+        (min_list[0] + max_list[0]) / 2,
+        1.3,
+        (min_list[2] + max_list[2]) / 2
+    ]
     dir_list: list = scene_json.get('front', [0, 0, 1])
     return {'pos': pos_list, 'direction': dir_list}
 
